cf8a08807072591ecfa3db99a25ba8b33499d036
rumeysagelgi <79201800+rumeysagelgi@users.noreply.github.com>
2024-03-12 21:16:23 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,13 +42,13 @@ export default function Home() {
       <input
         type="text"
         placeholder="Enter content - link, text or number"
-        className="text-primary text-base md:text-xs w-1/2 md:w-4/5 p-3 mt-10 mb-6 bg-secondary border-double border-b-4 border-b-primary border-r-4 border-r-primary rounded outline-none"
+        className="text-primary text-base md:text-xs w-1/2 md:w-4/5 p-3 md:p-1 mt-10 mb-6 bg-secondary border-double border-b-4 border-b-primary border-r-4 border-r-primary rounded outline-none"
         onChange={(e) => setInput(e.target.value)}
       ></input>
       <input
         type="number"
         placeholder="Enter size (default is 150px)"
-        className="text-primary text-base md:text-xs w-1/2 md:w-4/5 p-3 mt-4 mb-6 bg-secondary border-double border-b-4 border-b-primary border-r-4 border-r-primary rounded outline-none"
+        className="text-primary text-base md:text-xs w-1/2 md:w-4/5 p-3 md:p-1 mt-4 mb-6 bg-secondary border-double border-b-4 border-b-primary border-r-4 border-r-primary rounded outline-none"
         onChange={(e) => setSize(Math.min(4000, Math.max(40, e.target.value)))}
         max={4000}
         min={40}
@@ -56,23 +56,23 @@ export default function Home() {
       <input
         type="text"
         placeholder="Enter background color in HEX (default is FFFFFF)"
-        className="text-primary text-base md:text-xs w-1/2 md:w-4/5 p-3 mt-4 mb-6 bg-secondary border-double border-b-4 border-b-primary border-r-4 border-r-primary rounded outline-none"
+        className="text-primary text-base md:text-xs w-1/2 md:w-4/5 p-3 md:p-1 mt-4 mb-6 bg-secondary border-double border-b-4 border-b-primary border-r-4 border-r-primary rounded outline-none"
         onChange={(e) => setBackColor(e.target.value)}
       ></input>
       <input
         type="text"
         placeholder="Enter foreground color in HEX (default is 000000)"
-        className="text-primary text-base md:text-xs w-1/2 md:w-4/5 p-3 mt-4 mb-6 bg-secondary border-double border-b-4 border-b-primary border-r-4 border-r-primary rounded outline-none"
+        className="text-primary text-base md:text-xs w-1/2 md:w-4/5 p-3 md:p-1 mt-4 mb-6 bg-secondary border-double border-b-4 border-b-primary border-r-4 border-r-primary rounded outline-none"
         onChange={(e) => setForeColor(e.target.value)}
       ></input>
       <select
-        className="text-primary text-base md:text-xs w-1/2 md:w-4/5 p-3 mt-4 mb-6 bg-secondary border-double border-b-4 border-b-primary border-r-4 border-r-primary rounded outline-none"
+        className="text-primary text-base md:text-xs w-1/2 md:w-4/5 p-3 md:p-1 mt-4 mb-6 bg-secondary border-double border-b-4 border-b-primary border-r-4 border-r-primary rounded outline-none"
         onChange={(e) => setLevel(e.target.value)}>
-        <option value="" disabled selected className="p-3">Select error correction level (default is M)</option>
-        <option value="L" className="p-3">Low 7%</option>
-        <option value="M" className="p-3">Medium 15%</option>
-        <option value="Q" className="p-3">Quartile 25%</option>
-        <option value="H" className="p-3">High 30%</option>
+        <option value="" disabled selected>Select error correction level (default is M)</option>
+        <option value="L">Low 7%</option>
+        <option value="M">Medium 15%</option>
+        <option value="Q">Quartile 25%</option>
+        <option value="H">High 30%</option>
       </select>
       <button
         className="bg-primary text-secondary text-2xl md:text-lg font-dosis uppercase mt-10 mb-10 p-4 rounded border-dashed border-secondary border-2 inline-flex hover:opacity-80"
